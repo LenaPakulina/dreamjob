2040bd3aa8e80968700bc44c91c5e19d90d07d48
Elena Pakulina <el.pakulina@yandex.ru>
2024-04-04 09:26:31 +0500

    4. Thymeleaf, Циклы. [#504841 #489563]

--- src/main/java/ru/job4j/model/Vacancy.java
+++ src/main/java/ru/job4j/model/Vacancy.java
@@ -61,7 +61,7 @@ public class Vacancy {
             return false;
         }
         Vacancy vacancy = (Vacancy) o;
-        return id == vacancy.id && Objects.equals(title, vacancy.title) && Objects.equals(description, vacancy.description) && Objects.equals(creationDate, vacancy.creationDate);
+        return id == vacancy.id;
     }
 
     @Override
